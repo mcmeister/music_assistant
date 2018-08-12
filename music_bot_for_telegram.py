0758603c1a23fa7ba7b71609557c5aa4a074e3d3
mcmeister <34596749+mcmeister@users.noreply.github.com>
2018-08-12 23:19:44 +0700

    Update music_bot_for_telegram.py

--- music_bot_for_telegram.py
+++ music_bot_for_telegram.py
@@ -1,1 +1,11 @@
-print('Music Bot for Telegram \n version 0.0')
+##We're going to find music track by artist - name
+##Download it and upload to Telegram channel... 
+
+from googlesearch import search
+
+xx = input('Enter Artist - Name or Enter to finish: ')
+
+query = xx + '.mp3'
+mp3 = search(query, stop=50)
+for url in mp3:
+    print(url)
