95ebbe7245ff84991ec9c6764710bf318bca06e2
mcmeister <34596749+mcmeister@users.noreply.github.com>
2018-08-13 12:47:44 +0700

    rename

---
+++ music_assistant_for_telegram.py
@@ -0,0 +1,16 @@
+## We're going to search for music
+## Download as mp3-file
+## And upload to Telegram channel
+## Let's Go! :)
+## 12.08.2018 @ 4:03
+
+from googlesearch import search
+from html.parser import HTMLParser
+import urllib.request as urllib2
+
+a = input('Artist: ')
+b = input('Name: ')
+
+query = (a + ' ' + b + '.mp3')
+for url in search(query, stop=10):
+     print(url)
