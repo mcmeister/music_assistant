96aaf3e4c93f3b1ca7be1dc6e69f02d12ee3d44a
mcmeister <34596749+mcmeister@users.noreply.github.com>
2018-08-13 12:37:25 +0700

    advance

--- music_bot_for_telegram.py
+++ music_bot_for_telegram.py
@@ -1,1 +1,19 @@
-print('Music Bot for Telegram \n version 0.0')
+## We're going to search for music
+## Download as mp3-file
+## And upload to Telegram channel
+## Let's Go! :)
+## 12.08.2018 @ 4:03
+
+from googlesearch import search
+from html.parser import HTMLParser
+import urllib.request as urllib2
+
+a = input('Исполнитель: ')
+b = input('Название: ')
+
+query = (a + ' ' + b + '.mp3')
+for url in search(query, stop=10):
+     print(url)
+     
+class MyHTMLParser(HTMLParser):
+     parser.feed(query)
