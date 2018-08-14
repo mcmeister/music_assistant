3b5fe8c21bb578664056fa39602ab75f74237921
mcmeister <34596749+mcmeister@users.noreply.github.com>
2018-08-15 05:01:46 +0700

    advance

--- music_assistant_parser.py
+++ music_assistant_parser.py
@@ -1,12 +1,17 @@
 ## Music Assistant HTML parser
 
 import requests
+import string
 
 a = input('Artist: ')
 b = input('Name: ')
 c = input('Remix("Enter" for blank): ')
+s = str()
 
-query = (a + '_' + b + '_' + c)
-response = requests.get('http://mp3guild.com/mp3/' + query)
-song = response.url
-print(song)
+if c == s:
+    query = (a + '_-_' + b)
+else:
+    query = (a + '_-_' + b + '_-_' + c)
+
+response = requests.get('http://mp3guild.com/mp3/' + query + '.html')
+print(response.url)
