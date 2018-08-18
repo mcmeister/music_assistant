57a04faae8087d5fb3806675cdc96e1ceaf39948
mcmeister <34596749+mcmeister@users.noreply.github.com>
2018-08-18 13:06:00 +0700

    Update music_assistant_parser.py

--- music_assistant_parser.py
+++ music_assistant_parser.py
@@ -12,8 +12,7 @@ if c == s:
 else:
     query = (a + '_-_' + b + '_-_' + c)
 
-response = requests.get('http://mp3guild.com/mp3/'\
-                        + query + '.html')
+url = 'http://mp3guild.com/mp3/' + query + '.html'
 r = requests.get(url)
 with open('request.html', 'w') as output_file:
     output_file.write(r.text.encode('cp1251'))
