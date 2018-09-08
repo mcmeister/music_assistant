282ab38cc609c1ce5c105ac501f5e94e56ce353b
mcmeister <34596749+mcmeister@users.noreply.github.com>
2018-09-08 23:14:43 +0700

    comments

--- music_assistant_for_telegram.py
+++ music_assistant_for_telegram.py
@@ -9,29 +9,34 @@ import re
 from bs4 import BeautifulSoup
 import wget
 
+#user input section
 artistName = input('Artist: ')
 songName = input('Name: ')
-mixName = input('Mix(Blank for Original): ')
-blank = str()
+mixVer = input('Mix Version(Blank for Original): ')
+blank = str() #string variable (space)
 
+#appropriate query (including or excluding Version)
 if mixName == blank:
     query = (artistName + '_-_' + songName)
 else:
     query = (artistName + '_-_' + songName + '_-_' + mixName)
 
+#headers to access website
 headers = {
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:45.0) Gecko/20100101 Firefox/45.0'
       }
 
+#appropriate url-string to access website's Search
 url = 'http://mp3guild.com/mp3/'+query+'.html'
 
+#save Search results to Text-File
 response = requests.get(url, headers=headers, stream=True)
-
 handle = open('parse.txt', "wb")
 for chunk in response.iter_content(chunk_size=512):
     if chunk:
         handle.write(chunk)
 
+#parse & download mp3-link from Text-File
 with open("parse.txt", "r") as fp:
     soup = BeautifulSoup(fp, 'html.parser')
     link = soup.find(href=re.compile("dl.php?"))
