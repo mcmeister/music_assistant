e4e86dfa52f108552a15468fd3b62379771455fb
mcmeister <34596749+mcmeister@users.noreply.github.com>
2018-09-08 18:01:11 +0700

    Music Assistant progress

--- music_assistant_for_telegram.py
+++ music_assistant_for_telegram.py
@@ -4,18 +4,36 @@
 ## Let's Go! :)
 ## 12.08.2018 @ 4:03
 
-from googlesearch import search
 import requests
+import re
+from bs4 import BeautifulSoup
+import wget
 
-a = input('Artist: ')
-b = input('Name: ')
-c = input('Remix("Enter" for blank): ')
-s = str()
+artistName = input('Artist: ')
+songName = input('Name: ')
+mixName = input('Mix(Blank for Original): ')
+blank = str()
 
-if c == s:
-    query = (a + '_-_' + b)
+if mixName == blank:
+    query = (artistName + '_-_' + songName)
 else:
-    query = (a + '_-_' + b + '_-_' + c)
+    query = (artistName + '_-_' + songName + '_-_' + mixName)
 
-for url in search(query, stop=20):
-    print(url)
+headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:45.0) Gecko/20100101 Firefox/45.0'
+      }
+
+url = 'http://mp3guild.com/mp3/'+query+'.html'
+
+response = requests.get(url, headers=headers, stream=True)
+
+handle = open('parse.txt', "wb")
+for chunk in response.iter_content(chunk_size=512):
+    if chunk:
+        handle.write(chunk)
+
+with open("parse.txt", "r") as fp:
+    soup = BeautifulSoup(fp, 'html.parser')
+    link = soup.find(href=re.compile("dl.php?"))
+    file = link.get('href')
+    wget.download(file)
