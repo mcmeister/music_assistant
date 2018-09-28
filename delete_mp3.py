03a0ad95743e081cedc8603ec821725d87248f1a
mcmeister <farmazon.ua@gmail.com>
2018-09-28 19:51:17 +0700

    Default Changelist

--- delete_mp3.py
+++ delete_mp3.py
@@ -1,11 +1,20 @@
-import glob, os
+import os
+import glob
 
-## Delete Downloaded Mp3-File from a Disk
 
-os.chdir("/temp/")
-for file in glob.glob("*.mp3"):
-    if os.path.exists(file):
-        os.remove(file)
-        print('Deleted: ' + file)
+# Delete Downloaded Files from a Disk
+
+os.chdir("/tmp/")
+for file_tmp in glob.glob("*.tmp"):
+    if os.path.exists(file_tmp):
+        os.remove(file_tmp)
+        print('Deleted: ' + str(file_tmp))
+    else:
+        print("The file does not exist")
+
+for file_mp3 in glob.glob("*.mp3"):
+    if os.path.exists(file_mp3):
+        os.remove(file_mp3)
+        print('Deleted: ' + str(file_mp3))
     else:
         print("The file does not exist")
--- telegram_assistant_main.py
+++ telegram_assistant_main.py
@@ -1,4 +1,4 @@
-'''
+"""
 We're going to search for the music(done)
 Download track as mp3-file(done)
 And upload it to Telegram channel(done)
@@ -6,9 +6,16 @@ Let's Go! :)
 12.08.2018 @ 4:03
 All done!
 09.09.2018 @ 18:09
-'''
+"""
 
-## Banner Section
+import re
+import requests
+import telebot
+import wget
+from bs4 import BeautifulSoup
+from pyshorteners import Shortener
+from requests.packages.urllib3 import HTTPConnectionPool
+
 
 banner = '''
 --------------------------------------------------------------------------------------------------
@@ -21,39 +28,24 @@ banner = '''
          FOR TELEGRAM           v.1.01b         by Viacheslav Vorotilin aka "music meister"      
 --------------------------------------------------------------------------------------------------
 '''
-print(banner)
+print(banner + '\n')
 
-## Module Section
-
-import re
-import wget
-import telebot
-import requests
-from bs4 import BeautifulSoup
-from pyshorteners import Shortener
-
-## User Input Section
-
-artistName = input('Artist: ')                  ## Variable for Artist Name
-songName = input('Name: ')                      ## Variable for Song Name
-mixName = input('Mix Name: ')                   ## Variable for Remix Name
-blankInput = str()                              ## String Variable for Blank Input
-
-## Programm Input Section
+artistName = input('Artist: ')  # Variable for Artist Name
+songName = input('Name: ')      # Variable for Song Name
+mixName = input('Mix Name: ')   # Variable for Remix Name
+blankInput = str()              # String Variable for Blank Input
 
 '''
-plusInput = '+'                                 ## Variable for a Plus [+]
-underInput = '_'                                ## Variable for an Underscore [_]
+plusInput = '+'                 # Variable for a Plus [+]
+underInput = '_'                # Variable for an Underscore [_]
 '''
 
-spaceInput = ' '                                ## Variable for a Space [ ]
-hyphenInput = '-'                               ## Variable for a Hyphen [-]
-codeOpen = '<code>'                             ## Variable for Text-Formatting
-codeClose = '</code>'                           ## Variable for Text-Formatting
-boldOpen = '<b>'                                ## Variable for Text-Formatting
-boldClose = '</b>'                              ## Variable for Text-Formatting
-
-## Appropriate to Website's Search Query: Including or Excluding Remix Version
+spaceInput = ' '                # Variable for a Space [ ]
+hyphenInput = '-'               # Variable for a Hyphen [-]
+codeOpen = '<code>'             # Variable for Text-Formatting
+codeClose = '</code>'           # Variable for Text-Formatting
+boldOpen = '<b>'                # Variable for Text-Formatting
+boldClose = '</b>'              # Variable for Text-Formatting
 
 if mixName == blankInput:
     query = (artistName + spaceInput + songName)
@@ -63,98 +55,52 @@ elif artistName == blankInput:
     query = (songName + spaceInput + mixName)
 else:
     query = (artistName + spaceInput + songName + spaceInput + mixName)
-print(query + "\n")
 
-## Headers to Access Website
+if mixName == blankInput:
+    text = (codeOpen + artistName + spaceInput + hyphenInput + spaceInput + songName + codeClose)
+else:
+    text = (codeOpen + artistName + spaceInput + hyphenInput + spaceInput + songName
+            + spaceInput + '(' + mixName + ')' + codeClose)
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:45.0) \
     Gecko/20100101 Firefox/45.0'
 }
 
-## Appropriate URL-String to Access Website's Search
-
 url = 'https://mp3cc.biz/search/f/' + query + '/'
 
-## Save Search Results to Text-File
+with open('parse.txt', 'wb') as lf:
+    response = requests.get(url, headers=headers, stream=True)
+    lf.write(response.content)
+    lf.close()
 
-with open('parse.txt', "wb") as lf:
-    try:
-        response = requests.get(url, headers=headers, stream=True)
-        lf.write(response.content)
-        lf.close()
-    except:
-        pass
-
-## Parse Mp3-Link from Text-File
-
-with open('parse.txt', "r", encoding='UTF-8') as fp:
-    try:
-        soup = BeautifulSoup(fp, 'html.parser')
-        link = soup.find(href=re.compile("download"))
-        file = link.get('href')
-        print("Long URL => " + file + "\n")
-    except:
-        pass
-
-## shrinkApp - URL-Shortener Section
-
-class shrinkApp(Shortener):
-    def __init__(self):
-        try:
-            self.option = int(1)
-            self.url = str(file)
-            self.shortener = Shortener('Tinyurl')
-        except:
-            pass
-
-        if self.option == 1:
-            self.shrinkTheUrl()
-        else:
-            pass
-
-    def shrinkTheUrl(self):
-        self.shrinkUrl = self.shortener.short(self.url)
-
-## Download Mp3-File with Tinyurl
-
-        print("Downloading File via Short URL => " + self.shrinkUrl + "\n")
-        mp3 = wget.download(self.shrinkUrl, out='/temp/')
-        print('File Downloaded!' + "\n")
-
-## Telegram Bot Section
-
-        TOKEN = 'MY_TOKEN'
-        tb = telebot.TeleBot(TOKEN)
-        chat_id = '@my_channel'
-        audio = open(mp3, 'rb')
-
-## Send Audio File to Telegram Channel
-
-        tb.send_message(chat_id, text='<i>Uploading New Music...</i>', parse_mode='HTML')
-        print('Uploading File to Telegram Channel...' + "\n")
-
-        tb.send_audio(chat_id, audio)
-
-        tb.send_message(chat_id, text='<b>Music Uploaded!</b>', parse_mode='HTML')
-        print('File Uploaded!' + "\n")
-
-## Send Message to Telegram Channel
-
-        tb.send_message(chat_id, text=text, parse_mode='HTML')
-
-app = shrinkApp()
-
-## Appropriate Text Message to Post After Upload Completed
-
-if mixName == blankInput:
-    text = (codeOpen + artistName + spaceInput + hyphenInput + spaceInput + songName + codeClose)
-else:
-    text = {
-        codeOpen + artistName + spaceInput + hyphenInput + spaceInput + songName \
-        + spaceInput + '(' + mixName + ')' + codeClose
-    }
-
-## Delete Downloaded Mp3-File
+with open('parse.txt', 'r', encoding='UTF-8') as p:
+    s = BeautifulSoup(p, 'html.parser')
+    link = s.find(href=re.compile('download'))
+    print(type(link))
+    get_link = link.get('href')
+try:
+    shorten = Shortener('Tinyurl')
+    shrink_url = shorten.short(get_link)
+    print('Downloading: ' + '(' + artistName + spaceInput + hyphenInput + spaceInput + songName + ')' +
+          ' via Short URL => ' + shrink_url + '\n')
+    mp3 = wget.download(shrink_url, out='/tmp/')
+    print(mp3 + ' Downloaded!' + '\n')
+    chat_id = '@testing_now'
+    token = '658217975:AAEmtIoL3SX-Cf8budKCQHpd99BDNlEMnRg'
+    tb = telebot.TeleBot(token)
+    user = tb.get_me()
+    print(user)
+    audio = open(mp3, 'rb')
+    tb.send_audio(chat_id, audio)
+    print('Uploading File to Telegram Channel...\n')
+    print('File Uploaded!\n')
+    tb.send_message(chat_id, text)
+    print("Found: " + artistName + hyphenInput + songName + '\n')
+    print("Downloaded: " + artistName + hyphenInput + songName + '\n')
+    print("Uploaded to: " + chat_id + '\n')
+except HTTPConnectionPool(host='tinyurl.com', port=80) as e:
+    raise requests.exceptions.ReadTimeout
+    pass
 
 import delete_mp3
