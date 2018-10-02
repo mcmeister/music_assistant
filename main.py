bf4109ec3cec7beaa408e7e8102bf5b6667abfa8
mcmeister <farmazon.ua@gmail.com>
2018-10-02 11:00:48 +0700

    Merge branch 'master' of https://github.com/mcmeister/music_assistant
    # Conflicts:
    #	testing.py

--- main.py
+++ main.py
@@ -9,12 +9,17 @@ All done!
 """
 
 import re
-import requests
-import telebot
+import time
 import wget
+import telebot
 from bs4 import BeautifulSoup
 from pyshorteners import Shortener
-from mp3_tagger import VERSION_BOTH
+from requests import request
+from requests.exceptions import ReadTimeout
+from requests.exceptions import ConnectionError
+from requests.packages.urllib3 import HTTPConnectionPool
+from mp3_tagger import MP3File, VERSION_1, VERSION_2, VERSION_BOTH
+from http_request_randomizer.requests.proxy.requestProxy import RequestProxy
 
 banner = '''
 --------------------------------------------------------------------------------------------------
@@ -29,106 +34,99 @@ banner = '''
 '''
 print(banner + '\n')
 
-# UserInput
+if __name__ == '__main__':
 
-artistName = input('Artist: ')  # Variable for Artist Name
-songName = input('Name: ')      # Variable for Song Name
-mixName = input('Mix Name: ')   # Variable for Remix Name
-blankInput = str()              # String Variable for Blank Input
+    # UserInput
+    artistName = input('Artist: ')  # Variable for Artist Name
+    songName = input('Name: ')      # Variable for Song Name
+    mixName = input('Mix Name: ')   # Variable for Remix Name
+    blankInput = str()              # String Variable for Blank Input
 
-# ProgramInput
+    # ProgramInput
+    '''
+    plusInput = '+'                 # Variable for a Plus [+]
+    underInput = '_'                # Variable for an Underscore [_]
+    '''
+    spaceInput = ' '                # Variable for a Space [ ]
+    hyphenInput = '-'               # Variable for a Hyphen [-]
+    codeOpen = '<code>'             # Variable for Text-Formatting
+    codeClose = '</code>'           # Variable for Text-Formatting
+    boldOpen = '<b>'                # Variable for Text-Formatting
+    boldClose = '</b>'              # Variable for Text-Formatting
 
-'''
-plusInput = '+'                 # Variable for a Plus [+]
-underInput = '_'                # Variable for an Underscore [_]
-'''
-spaceInput = ' '                # Variable for a Space [ ]
-hyphenInput = '-'               # Variable for a Hyphen [-]
-codeOpen = '<code>'             # Variable for Text-Formatting
-codeClose = '</code>'           # Variable for Text-Formatting
-boldOpen = '<b>'                # Variable for Text-Formatting
-boldClose = '</b>'              # Variable for Text-Formatting
-
-# CuteQueries
-
-if mixName == blankInput:
-    query = (artistName + spaceInput + songName)
-elif songName == blankInput:
-    query = (artistName + spaceInput + mixName)
-elif artistName == blankInput:
-    query = (songName + spaceInput + mixName)
-else:
-    query = (artistName + spaceInput + songName + spaceInput + mixName)
-
-if mixName == blankInput:
-    text = (codeOpen + artistName + spaceInput + hyphenInput + spaceInput + songName + codeClose)
-else:
-    text = (codeOpen + artistName + spaceInput + hyphenInput + spaceInput + songName
-            + spaceInput + '(' + mixName + ')' + codeClose)
-
-# ProxySoon
-
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:45.0) \
-    Gecko/20100101 Firefox/45.0'
-}
-
-# SomeLink
-
-url = 'https://mp3cc.biz/search/f/' + query + '/'
-
-# MessBegins
-
-with open('parse.txt', 'wb') as lf:
-    try:
-        response = requests.get(url, headers=headers, stream=True)
-        lf.write(response.content)
-        lf.close()
-    except ConnectionRefusedError:
-        raise
+    # CuteQueries
+    if mixName == blankInput:
+        query = (artistName + spaceInput + songName)
+    elif songName == blankInput:
+        query = (artistName + spaceInput + mixName)
+    elif artistName == blankInput:
+        query = (songName + spaceInput + mixName)
     else:
-        pass
+        query = (artistName + spaceInput + songName + spaceInput + mixName)
 
-with open('parse.txt', 'r', encoding='UTF-8') as p:
-    s = BeautifulSoup(p, 'html.parser')
-    link = s.find(href=re.compile('download'))
-try:
-    get_link = link.get('href')
-except AttributeError:
-    raise
-else:
-    pass
+    if mixName == blankInput:
+        text = (codeOpen + artistName + spaceInput + hyphenInput + spaceInput + songName + codeClose)
+    else:
+        text = (codeOpen + artistName + spaceInput + hyphenInput + spaceInput + songName
+                + spaceInput + '(' + mixName + ')' + codeClose)
 
-shorten = Shortener('Tinyurl')
-shrink_url = shorten.short(get_link)
+    start = time.time()
+    req_proxy = RequestProxy()
+    print("Initialization took: {0} sec".format((time.time() - start)))
+    print("Size: {0}".format(len(req_proxy.get_proxy_list())))
+    print("ALL = {0} ".format(list(map(lambda x: x.get_address(), req_proxy.get_proxy_list()))))
 
-print('Downloading: ' + '(' + artistName + spaceInput + hyphenInput + spaceInput + songName + ')' +
-      ' via Short URL => ' + shrink_url + '\n')
+    url = 'https://mp3cc.biz/search/f/' + query + '/'
 
-mp3 = wget.download(get_link, out='/tmp/')
-print(mp3 + ' Downloaded!' + '\n')
-tags = mp3.get_tags(mp3)
-print(tags, type(tags))
-del tags
-mp3.set_version(VERSION_BOTH)
-mp3.artist = artistName
-mp3.song = songName
-mp3.album = mixName
-mp3.save()
-chat_id = '@my_id'
-token = 'my_token'
-tb = telebot.TeleBot(token)
-user = tb.get_me()
-print(user)
-audio = open(mp3, 'rb')
-print('Uploading File to Telegram Channel...\n')
-tb.send_audio(chat_id, audio)
-print('File Uploaded!\n')
-tb.send_message(chat_id, text)
-print("Found: " + artistName + hyphenInput + songName)
-print("Downloaded: " + artistName + hyphenInput + songName)
-print("Uploaded to: " + chat_id)
+    with open('parse.txt', 'wb') as f:
+        request = req_proxy.generate_proxied_request(url)
+        f.write(request.content)
 
-#MessCleans
+        print("Proxies Request Took: {0} sec => Status: {1}".format((time.time() - start), request.__str__()))
+        print("-> Going to sleep..")
+        time.sleep(10)
+
+    with open('parse.txt', 'r', encoding='UTF-8') as p:
+        s = BeautifulSoup(p, 'html.parser')
+        link = s.find(href=re.compile('download'))
+        get_link = link.get('href')
+
+        try:
+            shorten = Shortener('Tinyurl')
+            shrink_url = shorten.short(get_link)
+        except ReadTimeout as e:
+            raise ReadTimeout(e, request=request)
+        else:
+            pass
+
+    print('Downloading: ' + '(' + artistName + spaceInput + hyphenInput + spaceInput + songName + ')' +
+          ' via Short URL => ' + shrink_url + '\n')
+    file = req_proxy.generate_proxied_request(shrink_url)
+    f = wget.download(file, out='/tmp/')
+    print(file + ' Downloaded!' + '\n')
+
+    mp3 = MP3File(file)
+    art = mp3.artist
+    sng = mp3.song
+    alb = mp3.album
+    mp3.set_version(VERSION_BOTH)
+    mp3.artist = artistName
+    mp3.song = songName
+    mp3.album = 'Telegram'
+    mp3.save()
+    chat_id = '@testing_now'
+    token = '658217975:AAEmtIoL3SX-Cf8budKCQHpd99BDNlEMnRg'
+    tb = telebot.TeleBot(token)
+    user = tb.get_me()
+    print(user)
+
+    with open(file, 'rb') as audio:
+        print('Uploading File to Telegram Channel...\n')
+        tb.send_audio(chat_id, audio)
+        print('File Uploaded!\n')
+        tb.send_message(chat_id, text)
+        print("Found: " + artistName + hyphenInput + songName)
+        print("Downloaded: " + artistName + hyphenInput + songName)
+        print("Uploaded to: " + chat_id)
 
 import cleaner
