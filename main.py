0802037229a81ceb9c732aeb68d1aa4fa2de8564
mcmeister <34596749+mcmeister@users.noreply.github.com>
2018-11-20 11:37:34 +0700

    update queries

--- main.py
+++ main.py
@@ -15,7 +15,6 @@ import re
 import wget
 import telebot
 import requests
-import lxml.html
 from bs4 import BeautifulSoup
 from pyshorteners import Shortener
 from mp3_tagger import MP3File, VERSION_BOTH
@@ -51,10 +50,10 @@ underInput = '_'                    # Variable for an Underscore [_]
 '''
 spaceInput = ' '                    # Variable for a Space [ ]
 hyphenInput = '-'                   # Variable for a Hyphen [-]
-codeOpen = '<code>'                 # Variable for Text-Formatting
-codeClose = '</code>'               # Variable for Text-Formatting
-boldOpen = '<b>'                    # Variable for Text-Formatting
-boldClose = '</b>'                  # Variable for Text-Formatting
+codeOpen = '<code>'                 # Variable for Text-Formatting in Telegram
+codeClose = '</code>'               # Variable for Text-Formatting in Telegram
+boldOpen = '<b>'                    # Variable for Text-Formatting in Telegram
+boldClose = '</b>'                  # Variable for Text-Formatting in Telegram
 
 # cuteQueries
 
@@ -67,8 +66,7 @@ else:
     text = (codeOpen + artistName + spaceInput + hyphenInput + spaceInput + songName
             + '(' + mixName + ')' + codeClose)
     newName = ('/tmp/' + artistName + hyphenInput + songName + hyphenInput + mixName + '.mp3')
-
-print('\nWorking on Request: ' + query)    
+print('\nQuery: ' + query)
 
 # baseURL
 
@@ -79,65 +77,92 @@ url = 'https://mp3cc.biz/search/f/' + query + '/'
 req_proxy = RequestProxy()
 
 while not req_proxy.generate_proxied_request(url):
-    print('\nNext proxy for "Base URL"')
+    print('\nNext proxy for Base URL')
 else:
-    print('\nConnected to "Base URL!"')
+    print('\nConnected to Base URL!')
+    pass
+
+# saveToFile
+
+with open('parse.txt', 'wb') as f:
     response = requests.get(url)
+    f.write(response.content)
 
-page = response.content
-dom = lxml.html.fromstring(page)
+# parseFromFile
 
-links = dom.xpath('//div[@class="playlist-btn"]//a/@href')
-for link in links:
-    print(link)
+with open('parse.txt', 'r', encoding='UTF-8') as p:
+    s = BeautifulSoup(p, 'html.parser')
+    link = s.find(href=re.compile('download'))
+    get_link = link.get('href')
 
-    while not req_proxy.generate_proxied_request(link):
-        print('\nNext proxy for "Parsed URL"')
-    else:
-        print('\nConnected to Parsed "URL!"')
-        pass
+# shrinkParsedURL
 
-    print('\nDownloading: ' + query + '\n')
-    file = wget.download(link, out='/tmp')
-    print('\nDownloaded: ' + format(file))
+access_token = 'my_bitly_token'
+tinyurl_short = Shortener('Tinyurl')
+bitly_short = Shortener('Bitly', bitly_token=access_token)
+
+while not req_proxy.generate_proxied_request(get_link):
+    print('\nNext proxy for Parsed URL')
+else:
+    print('\nConnected to Parsed URL!')
+    pass
+
+shrink_url = bitly_short.short(get_link)
+if shrink_url:
+    print('\nBitLy: ' + str(shrink_url))
+else:
+    shrink_url = tinyurl_short.short(get_link)
+    print('\nTinyurl: ' + str(shrink_url))
+
+# downloadShrinkURL
+
+while not req_proxy.generate_proxied_request(shrink_url):
+    print('\nNext proxy for Shrink URL')
+else:
+    print('\nConnected to Shrink URL!')
+    pass
+
+print('\nDownloading: ' + query + ' via Short URL --> ' + shrink_url)
+file = wget.download(shrink_url, out='/tmp')
+print('\nDownloaded!: ' + str(file))
 
 # editID3Tags
 
-    mp3 = MP3File(file)
-    mp3.set_version(VERSION_BOTH)
-    mp3.artist = artistName
-    mp3.song = songName
-    mp3.album = 'Telegram'
-    mp3.publisher = ''
-    mp3.save()
-    tags = mp3.get_tags()
-    print(tags)
+mp3 = MP3File(file)
+mp3.set_version(VERSION_BOTH)
+mp3.artist = artistName
+mp3.song = songName
+mp3.album = 'Telegram'
+mp3.publisher = ''
+mp3.save()
+tags = mp3.get_tags()
+print(tags)
 
 # telegramBot
 
-    print('\nNew Filename is: ' + newName)
-    os.rename(str(file), newName)
-    audio = open(newName, 'rb')
-    token = 'my_bot_token'
-    chat_id = '@my_chat_id'
-    tb = telebot.TeleBot(token)
-    tb_status = str(tb.get_me())
-    print('\nTeleBot Status: ' + tb_status)
+print('\nFilename will be: ' + newName)
+os.rename(str(file), newName)
+audio = open(newName, 'rb')
+token = 'my_bot_token'
+chat_id = '@my_chat_id'
+tb = telebot.TeleBot(token)
+tb_status = str(tb.get_me())
+print('\nStatus: ' + tb_status)
 
 # uploadFile
 
-    print('\nUploading File to Telegram Channel: ' + chat_id)
-    print('\nThe Caption is: ' + str(text))
-    send = tb.send_audio(chat_id, audio)
-    message_id = send.message_id
-    caption = str(text)
-    tb.edit_message_caption(caption, chat_id, message_id, parse_mode='HTML')
-    print('\nFile Uploaded!')
+print('\nUploading File to a Telegram Channel: ' + chat_id)
+print('\nMessage will be: ' + str(text))
+send = tb.send_audio(chat_id, audio)
+message_id = send.message_id
+caption = str(text)
+tb.edit_message_caption(caption, chat_id, message_id, parse_mode='HTML')
+print('\nFile Uploaded!')
 
 # statusImprint
 
-    print('\nFound: ' + query)
-    print('Downloaded: ' + file)
-    print('Uploaded to: ' + chat_id)
+print('\nFound: ' + query)
+print('\nDownloaded: ' + query)
+print('\nUploaded to: ' + chat_id)
 
-    import cleaner
+import cleaner
