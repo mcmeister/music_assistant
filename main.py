05cc7bdbb60302a49a5e25603eb00a9590b19505
mcmeister <34596749+mcmeister@users.noreply.github.com>
2024-06-05 12:03:43 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,3 @@
-"""
-We're going to search for the music(done)
-Download track as mp3-file(done)
-And upload it to Telegram channel(done)
-Let's Go! :)
-12.08.2018 @ 4:03
-All done!
-09.09.2018 @ 18:09
-"""
-
-# importInput
-
 import os
 import re
 import wget
@@ -19,9 +7,12 @@ from bs4 import BeautifulSoup
 from pyshorteners import Shortener
 from mp3_tagger import MP3File, VERSION_BOTH
 from http_request_randomizer.requests.proxy.requestProxy import RequestProxy
+import logging
 
-# bannerInput
+# Setup logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
+# Banner
 banner = '''
 --------------------------------------------------------------------------------------------------
   __  __                 _                                   _         _                     _    
@@ -35,134 +26,126 @@ banner = '''
 '''
 print(banner)
 
-# userInput
+# User Input
+artist_name = input('Artist: ')
+song_name = input('Name: ')
+mix_name = input('Remixed by: ')
+blank_input = str()
 
-artistName = input('Artist: ')      # Variable for Artist Name
-songName = input('Name: ')          # Variable for Song Name
-mixName = input('Remixed by: ')     # Variable for DJ Name
-blankInput = str()                  # String Variable for Blank Input
+# Constants
+SPACE = ' '
+HYPHEN = '-'
+CODE_OPEN = '<code>'
+CODE_CLOSE = '</code>'
+BOLD_OPEN = '<b>'
+BOLD_CLOSE = '</b>'
 
-# programInput
-
-'''
-plusInput = '+'                     # Variable for a Plus [+]
-underInput = '_'                    # Variable for an Underscore [_]
-'''
-spaceInput = ' '                    # Variable for a Space [ ]
-hyphenInput = '-'                   # Variable for a Hyphen [-]
-codeOpen = '<code>'                 # Variable for Text-Formatting in Telegram
-codeClose = '</code>'               # Variable for Text-Formatting in Telegram
-boldOpen = '<b>'                    # Variable for Text-Formatting in Telegram
-boldClose = '</b>'                  # Variable for Text-Formatting in Telegram
-
-# cuteQueries
-
-if mixName == blankInput:
-    query = (artistName + spaceInput + songName)
-    text = (codeOpen + artistName + spaceInput + hyphenInput + spaceInput + songName + codeClose)
-    newName = ('/tmp/' + artistName + hyphenInput + songName + '.mp3')
+# Generate Query
+if mix_name == blank_input:
+    query = f"{artist_name} {song_name}"
+    text = f"{CODE_OPEN}{artist_name} - {song_name}{CODE_CLOSE}"
+    new_name = f"/tmp/{artist_name}-{song_name}.mp3"
 else:
-    query = (artistName + spaceInput + songName + spaceInput + mixName)
-    text = (codeOpen + artistName + spaceInput + hyphenInput + spaceInput + songName
-            + '(' + mixName + ')' + codeClose)
-    newName = ('/tmp/' + artistName + hyphenInput + songName + hyphenInput + mixName + '.mp3')
-print('\nQuery: ' + query)
+    query = f"{artist_name} {song_name} {mix_name}"
+    text = f"{CODE_OPEN}{artist_name} - {song_name} ({mix_name}){CODE_CLOSE}"
+    new_name = f"/tmp/{artist_name}-{song_name}-{mix_name}.mp3"
+logging.info(f"Query: {query}")
 
-# baseURL
+# Base URL
+url = f"https://mp3cc.biz/search/f/{query}/"
 
-url = 'https://mp3cc.biz/search/f/' + query + '/'
-
-# proxy_headersRequest
-
+# Proxy Request
 req_proxy = RequestProxy()
+response = None
 
-while not req_proxy.generate_proxied_request(url):
-    print('\nNext proxy for Base URL')
-else:
-    print('\nConnected to Base URL!')
-    pass
+while not response:
+    try:
+        response = req_proxy.generate_proxied_request(url)
+        logging.info('Connected to Base URL!')
+    except Exception as e:
+        logging.error(f"Failed to connect, retrying... Error: {e}")
 
-# saveToFile
-
+# Save to File
 with open('parse.txt', 'wb') as f:
     response = requests.get(url)
     f.write(response.content)
 
-# parseFromFile
-
+# Parse from File
 with open('parse.txt', 'r', encoding='UTF-8') as p:
-    s = BeautifulSoup(p, 'html.parser')
-    link = s.find(href=re.compile('download'))
+    soup = BeautifulSoup(p, 'html.parser')
+    link = soup.find(href=re.compile('download'))
     get_link = link.get('href')
 
-# shrinkParsedURL
-
+# Shorten URL
 access_token = 'my_bitly_token'
 tinyurl_short = Shortener('Tinyurl')
 bitly_short = Shortener('Bitly', bitly_token=access_token)
 
-while not req_proxy.generate_proxied_request(get_link):
-    print('\nNext proxy for Parsed URL')
-else:
-    print('\nConnected to Parsed URL!')
-    pass
+shrink_url = None
+response = None
 
-shrink_url = bitly_short.short(get_link)
-if shrink_url:
-    print('\nBitLy: ' + str(shrink_url))
-else:
+while not response:
+    try:
+        response = req_proxy.generate_proxied_request(get_link)
+        logging.info('Connected to Parsed URL!')
+    except Exception as e:
+        logging.error(f"Failed to connect, retrying... Error: {e}")
+
+try:
+    shrink_url = bitly_short.short(get_link)
+    logging.info(f'BitLy: {shrink_url}')
+except Exception as e:
+    logging.error(f"BitLy failed, trying TinyURL... Error: {e}")
     shrink_url = tinyurl_short.short(get_link)
-    print('\nTinyurl: ' + str(shrink_url))
+    logging.info(f'TinyURL: {shrink_url}')
 
-# downloadShrinkURL
+# Download File
+response = None
 
-while not req_proxy.generate_proxied_request(shrink_url):
-    print('\nNext proxy for Shrink URL')
-else:
-    print('\nConnected to Shrink URL!')
-    pass
+while not response:
+    try:
+        response = req_proxy.generate_proxied_request(shrink_url)
+        logging.info('Connected to Shrink URL!')
+    except Exception as e:
+        logging.error(f"Failed to connect, retrying... Error: {e}")
 
-print('\nDownloading: ' + query + ' via Short URL --> ' + shrink_url)
+logging.info(f'Downloading: {query} via Short URL --> {shrink_url}')
 file = wget.download(shrink_url, out='/tmp')
-print('\nDownloaded!: ' + str(file))
+logging.info(f'Downloaded: {file}')
 
-# editID3Tags
-
+# Edit ID3 Tags
 mp3 = MP3File(file)
 mp3.set_version(VERSION_BOTH)
-mp3.artist = artistName
-mp3.song = songName
+mp3.artist = artist_name
+mp3.song = song_name
 mp3.album = 'Telegram'
 mp3.publisher = ''
 mp3.save()
 tags = mp3.get_tags()
-print(tags)
+logging.info(tags)
 
-# telegramBot
-
-print('\nFilename will be: ' + newName)
-os.rename(str(file), newName)
-audio = open(newName, 'rb')
+# Telegram Bot
+logging.info(f'Filename will be: {new_name}')
+os.rename(file, new_name)
+audio = open(new_name, 'rb')
 token = 'my_bot_token'
 chat_id = '@my_chat_id'
 tb = telebot.TeleBot(token)
-tb_status = str(tb.get_me())
-print('\nStatus: ' + tb_status)
+tb_status = tb.get_me()
+logging.info(f'Status: {tb_status}')
 
-# uploadFile
-
-print('\nUploading File to a Telegram Channel: ' + chat_id)
-print('\nMessage will be: ' + str(text))
+# Upload File
+logging.info(f'Uploading File to Telegram Channel: {chat_id}')
+logging.info(f'Message will be: {text}')
 send = tb.send_audio(chat_id, audio)
 message_id = send.message_id
-caption = str(text)
+caption = text
 tb.edit_message_caption(caption, chat_id, message_id, parse_mode='HTML')
-print('\nFile Uploaded!')
+logging.info('File Uploaded!')
 
-# statusImprint
-
-print('\nFound: ' + query)
-print('\nDownloaded: ' + query)
-print('\nUploaded to: ' + chat_id)
+# Status Imprint
+logging.info(f'Found: {query}')
+logging.info(f'Downloaded: {query}')
+logging.info(f'Uploaded to: {chat_id}')
 
 import cleaner
